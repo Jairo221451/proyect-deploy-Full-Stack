0a989dee9ab8454366cedf74a0f2fb2223b5cd45
Jairo221451 <221451@unsaac.edu.pe>
2025-06-28 18:34:13 -0500

    Conectar DataBase

--- Backend/Database/connection.js
+++ Backend/Database/connection.js
@@ -1,10 +1,14 @@
+require('dotenv').config();
 const mysql = require('mysql2/promise');
+
 const connection = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'bd_ds',
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT || 3306,
   waitForConnections: true,
   connectionLimit: 10,
 });
+
 module.exports = connection;
