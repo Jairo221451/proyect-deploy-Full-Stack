5547210990476e67d0f400d1bc17fdcdd31facec
Jairo221451 <221451@unsaac.edu.pe>
2025-06-29 16:19:41 -0500

    Probar conexion con Railway

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,10 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   define: {
     'process.env.REACT_APP_BACKEND_URL': '"https://proyect-deploy-full-stack-production.up.railway.app/api"'
+  },
+  server: {
+    host: '0.0.0.0', // Permite conexiones externas
+    port: 5173
+  },
+  preview: {
+    host: '0.0.0.0',
+    port: 5173
   }
 })
