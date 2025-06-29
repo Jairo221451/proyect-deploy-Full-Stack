015399dd52a6be17ba9bd65e42112b8b11e4bb15
Jairo221451 <221451@unsaac.edu.pe>
2025-06-28 22:22:25 -0500

    Upload Frontend

--- Frontend/src/Pages/MisEmprendimientos.jsx
+++ Frontend/src/Pages/MisEmprendimientos.jsx
@@ -5,7 +5,7 @@ import { Card, Container, Row, Col, Button, Spinner, Alert, Modal } from 'react-
 // Importación de componentes de React Router para navegación y enlaces
 import { Link, useNavigate } from 'react-router-dom';
 // Cliente API personalizado para llamadas al backend
-import api from '../Services/api';
+import api from '../services/api';
 
 // Definición del componente principal
 const MisEmprendimientos = () => {
--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  define: {
+    'process.env.REACT_APP_BACKEND_URL': '"https://proyect-deploy-full-stack-production.up.railway.app/api"'
+  }
 })
