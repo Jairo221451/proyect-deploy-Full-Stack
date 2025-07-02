53141701a5c14025e0a44fe6de4f193de6b863c8
Jairo221451 <221451@unsaac.edu.pe>
2025-07-01 22:47:07 -0500

    Uncomment Railway production URL in CORS configuration

--- Backend/index.js
+++ Backend/index.js
@@ -11,7 +11,7 @@ app.use(cors({
     'http://localhost:3001', 
     'http://localhost:5173',
     'http://localhost:29575',
-    //'https://proyect-deploy-full-stack-production-45cc.up.railway.app' 
+    'https://proyect-deploy-full-stack-production-45cc.up.railway.app' 
   ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
